8bf5cfc3d5c6310e14d6ca600537b66a0c242871
ulysses-ck <ulisesapaza.280@gmail.com>
2023-11-15 18:10:33 -0300

    feat: create class Obra and defining their methods

--- modelo_orm.py
+++ modelo_orm.py
@@ -79,3 +79,34 @@ class TipoDestacada(BaseModel):
 
     class Meta:
         db_table = "tipos_destacada"
+
+
+class Obra(BaseModel):
+    tipo_entorno = ForeignKeyField(TipoEntorno, backref="tipos_entorno")
+
+    def nuevo_proyecto():
+        pass
+
+    def iniciar_contratacion():
+        pass
+
+    def adjudicar_obra():
+        pass
+
+    def iniciar_obra():
+        pass
+
+    def actualizar_porcentaje_avance():
+        pass
+
+    def incrementar_plazo():
+        pass
+
+    def incrementar_mano_obra():
+        pass
+
+    def finalizar_obra():
+        pass
+
+    def rescindir_obra():
+        pass
