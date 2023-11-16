1cb400710dc938e6cc4df3eba7fb3410552e8281
SIA <diegoamezaga@gmail.com>
2023-11-15 21:52:26 -0300

    se saca obras publicas

--- modelo_orm.py
+++ modelo_orm.py
@@ -84,7 +84,7 @@ class TipoDestacada(BaseModel):
 
 class Obra(BaseModel):
     # agrego atributos
-    id = IntegerField
+    id = IntegerField(unique=True)
     tipo_entorno = ForeignKeyField(TipoEntorno, backref="tipo_entorno")
     nombre = CharField
     etapa = ForeignKeyField(TipoEtapa, backref="tipo_etapa")
@@ -122,6 +122,13 @@ class Obra(BaseModel):
     expediente_numero = IntegerField
     estudio_ambiental_descarga = CharField
     financiamiento = CharField
+    
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = "obras_publicas"
+    
 
     def nuevo_proyecto():
         pass
@@ -149,45 +156,3 @@ class Obra(BaseModel):
 
     def rescindir_obra():
         pass
-
-class Obras_Publicas(BaseModel):
-    id=IntegerField(unique = True)
-    entorno = CharField
-    nombre = CharField
-    etapa = CharField
-    tipo = CharField
-    area_responsable = CharField
-    descripcion = CharField
-    monto_contratos = DoubleField
-    comuna = IntegerField
-    barrio = CharField
-    direccion = CharField
-    lat = DoubleField
-    lgn = DoubleField
-    fecha_inicio = DateTimeField
-    fecha_fin_inicial = DateTimeField
-    plazo_meses = IntegerField
-    porcentaje_avance = IntegerField
-    imagen_1 = CharField
-    imagen_2 = CharField
-    imagen_3 = CharField
-    imagen_4 = CharField
-    licitacion_oferta_empresa = CharField
-    licitacion_anio = IntegerField
-    contratacion_tipo = CharField
-    nro_contratacion = CharField(unique=True)
-    cuit_contratista = IntegerField(unique=True)
-    beneficiarios = CharField
-    mano_obra = CharField
-    compromiso = CharField
-    destacada = BooleanField
-    ba_elige = CharField
-    link_interno = CharField
-    pliego_descarga = CharField
-    expediente_numero = IntegerField(unique=True)
-    estudio_ambiental_descarga = CharField
-    financiamiento = DoubleField
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        db_table = "Obras Publicas"
