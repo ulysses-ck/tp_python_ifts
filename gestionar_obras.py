0ae29dda73439ef396f169c55510a53d4eca2792
SIA <diegoamezaga@gmail.com>
2023-11-15 21:33:08 -0300

    cambio en modelo obras publicas

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -42,3 +42,31 @@ class GestionarObras (Model):
 
 	def nueva_obra(self):
 		pass
+class Obra():
+    
+    def nuevo_proyecto():
+        pass
+    
+    def iniciar_contratacion():
+        pass
+    
+    def adjudicar_obra():
+        pass
+    
+    def iniciar_obra():
+        pass
+    
+    def actualizar_porcentaje_avance():
+        pass
+    
+    def incrementar_plazo():
+        pass
+    
+    def incrementar_mano_obra():
+        pass
+    
+    def finalizar_obra():
+        pass
+    
+    def rescindir_obra():
+        pass
--- modelo_orm.py
+++ modelo_orm.py
@@ -1,4 +1,5 @@
 from peewee import *
+import datetime
 
 
 class TipoEntorno(BaseModel):
@@ -79,3 +80,45 @@ class TipoDestacada(BaseModel):
 
     class Meta:
         db_table = "tipos_destacada"
+
+class Obras_Publicas(BaseModel):
+    id=IntegerField(unique = True)
+    entorno = CharField
+    nombre = CharField
+    etapa = CharField
+    tipo = CharField
+    area_responsable = CharField
+    descripcion = CharField
+    monto_contratos = DoubleField
+    comuna = IntegerField
+    barrio = CharField
+    direccion = CharField
+    lat = DoubleField
+    lgn = DoubleField
+    fecha_inicio = DateTimeField
+    fecha_fin_inicial = DateTimeField
+    plazo_meses = IntegerField
+    porcentaje_avance = IntegerField
+    imagen_1 = CharField
+    imagen_2 = CharField
+    imagen_3 = CharField
+    imagen_4 = CharField
+    licitacion_oferta_empresa = CharField
+    licitacion_anio = IntegerField
+    contratacion_tipo = CharField
+    nro_contratacion = CharField(unique=True)
+    cuit_contratista = IntegerField(unique=True)
+    beneficiarios = CharField
+    mano_obra = CharField
+    compromiso = CharField
+    destacada = BooleanField
+    ba_elige = CharField
+    link_interno = CharField
+    pliego_descarga = CharField
+    expediente_numero = IntegerField(unique=True)
+    estudio_ambiental_descarga = CharField
+    financiamiento = DoubleField
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        db_table = "Obras Publicas"
