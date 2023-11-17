f07ff56481b1a93c110109840cb85336a23da5fe
ulysses-ck <ulisesapaza.280@gmail.com>
2023-11-16 21:42:06 -0300

    feat: remove unnecesary fields and inherit GestionarObras from abstract class

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,29 +1,22 @@
 import pandas as pd
 from peewee import *
+from abc import ABCMeta
 
+class GestionarObras(metaclass=ABCMeta):
+	df_obras_publicas = None
+	sqlite_db_obras = None
 
-class GestionarObras():
-	def __init__(self) -> None:
-		self.__df_obras_publicas = self.extraer_datos()
-		self.__sqlite_db_obras = self.conectar_db()
-
-	@property
-	def df_obras_publicas(self):
-		return self.__df_obras_publicas
-
-	@property
-	def sqlite_db_obras(self):
-		return self.__sqlite_db_obras
-
-	def extraer_datos(self):
+	@classmethod
+	def extraer_datos(cls):
 		try:
 			df = pd.read_csv("./observatorio-de-obras-urbanas.csv")
-			return df
+			cls.df = df
 		except FileNotFoundError as e:
 			print("El archivo no existe o la ubicación del mismo es incorrecta", e)
 			return None
 
-	def conectar_db(self):
+	@classmethod
+	def conectar_db(cls):
 		sqlite_db_obras = SqliteDatabase("./obras_urbanas.db")
 		try:
 			sqlite_db_obras.connect()
@@ -32,14 +25,18 @@ class GestionarObras():
 			print("Se ha generado un error en la conexión a la BD.", e)
 			exit()
 
-	def mapear_orm(self):
-		pass
+	@classmethod
+	def mapear_orm(cls):
+		print("mapear orm")
 
-	def limpiar_datos(self):
-		self.df_obras_publicas.dropna(subset=["entorno", "nombre", "etapa", "tipo", "area_responsable", "monto_contrato", "comuna", "barrio", "direccion", "fecha_inicio", "porcenta_avance", "licitacion_oferta_empresa", "licitacion_anio", "contratacion_tipo", "nro_contratacion", "cuit_contratista", "mano_obra"], axis=0, inplace=True)
+	@classmethod
+	def limpiar_datos(cls, df):
+		cls.df = df.dropna(subset=["entorno", "nombre", "etapa", "tipo", "area_responsable", "monto_contrato", "comuna", "barrio", "direccion", "fecha_inicio", "porcenta_avance", "licitacion_oferta_empresa", "licitacion_anio", "contratacion_tipo", "nro_contratacion", "cuit_contratista", "mano_obra"], axis=0, inplace=True)
 
-	def cargar_datos(self):
-		pass
+	@classmethod
+	def cargar_datos(cls):
+		print("cargar datos")
 
-	def nueva_obra(self):
-		pass
+	@classmethod
+	def nueva_obra(cls):
+		print("nueva obra")
--- modelo_orm.py
+++ modelo_orm.py
@@ -2,94 +2,94 @@ from peewee import *
 
 sqlite_db = SqliteDatabase("obras_urbanas.db")
 try:
-    sqlite_db.connect()
+  sqlite_db.connect()
 except OperationalError as e:
-    print("No se pudo realizar la conexion")
-    exit()
+  print("No se pudo realizar la conexion")
+  exit()
 
 
 class BaseModel(Model):
-    class Meta:
-      database=sqlite_db
+  class Meta:
+    database=sqlite_db
 
 class TipoEntorno(BaseModel):
-    nombre = CharField(unique=True)
+  nombre = CharField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_entorno"
+  class Meta:
+    db_table = "tipos_entorno"
 
 
 class TipoEtapa(BaseModel):
-    nombre = CharField(unique=True)
+  nombre = CharField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_etapa"
+  class Meta:
+    db_table = "tipos_etapa"
 
 
 class TipoTipo(BaseModel):
-    nombre = CharField(unique=True)
+  nombre = CharField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_tipo"
+  class Meta:
+    db_table = "tipos_tipo"
 
 
 class TipoAreaResponsable(BaseModel):
-    nombre = CharField(unique=True)
+  nombre = CharField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_area_responsable"
+  class Meta:
+    db_table = "tipos_area_responsable"
 
 
 class TipoComuna(BaseModel):
-    nombre = IntegerField(unique=True)
+  nombre = IntegerField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_comuna"
+  class Meta:
+    db_table = "tipos_comuna"
 
 
 class TipoBarrio(BaseModel):
-    nombre = CharField(unique=True)
+  nombre = CharField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_barrio"
+  class Meta:
+    db_table = "tipos_barrio"
 
 
 class TipoCompromiso(BaseModel):
-    nombre = BooleanField(unique=True)
+  nombre = BooleanField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_compromiso"
+  class Meta:
+    db_table = "tipos_compromiso"
 
 
 class TipoDestacada(BaseModel):
-    nombre = BooleanField(unique=True)
+  nombre = BooleanField(unique=True)
 
-    def __str__(self):
-        return self.nombre
+  def __str__(self):
+    return self.nombre
 
-    class Meta:
-        db_table = "tipos_destacada"
+  class Meta:
+    db_table = "tipos_destacada"
 
 
 class Obra(BaseModel):
@@ -106,16 +106,10 @@ class Obra(BaseModel):
   comuna = ForeignKeyField(TipoComuna, backref="tipo_comuna")
   barrio = ForeignKeyField(TipoBarrio, backref="tipo_barrio")
   direccion = CharField(null=True)
-  lat = DoubleField(null=True)
-  lng = DoubleField(null=True)
   fecha_inicio = DateTimeField()
   fecha_fin_inicial = DateTimeField(null=True)
   plazo_meses = IntegerField(null=True)
   porcentaje_avance = FloatField()
-  imagen1 = CharField(null=True)
-  imagen2 = CharField(null=True)
-  imagen3 = CharField(null=True)
-  imagen4 = CharField(null=True)
   licitacion_oferta_empresa = CharField()
   licitacion_anio = IntegerField()
   contratacion_tipo = CharField()
@@ -125,44 +119,40 @@ class Obra(BaseModel):
   mano_obra = CharField()
   compromiso = ForeignKeyField(TipoCompromiso, backref="tipo_compromiso")
   destacada = ForeignKeyField(TipoDestacada, backref="tipo_destacada")
-  ba_elige = CharField(null=True)
-  link_interno = CharField(null=True)
-  pliego_descarga = CharField(null=True)
   expediente_numero = IntegerField(unique=True, null=True)
-  estudio_ambiental_descarga = CharField(null=True)
   financiamiento = CharField(null=True)
 
   def __str__(self):
-      return self.nombre
+    return self.nombre
 
   class Meta:
     db_table = "obras_publicas"
 
   def nuevo_proyecto(self):
-      pass
+    print("nuevo proyecto")
 
   def iniciar_contratacion(self):
-      pass
+    print("iniciar contratacion")
 
   def adjudicar_obra(self):
-      pass
+    print("adjudicar obra")
 
   def iniciar_obra(self):
-      pass
+    print("iniciar obra")
 
   def actualizar_porcentaje_avance(self):
-      pass
+    print("actualizar porcentaje avance")
 
   def incrementar_plazo(self):
-      pass
+    print("incrementar plazo")
 
   def incrementar_mano_obra(self):
-      pass
+    print("incrementar obra")
 
   def finalizar_obra(self):
-      pass
+    print("finalizar obra")
 
   def rescindir_obra(self):
-      pass
+    print("rescindir obra")
 
 sqlite_db.create_tables([TipoAreaResponsable, TipoBarrio, TipoCompromiso, TipoComuna, TipoDestacada, TipoEntorno, TipoEtapa, TipoTipo, Obra])
