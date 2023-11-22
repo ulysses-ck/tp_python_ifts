0cb8ef0cbafe077b984687a02175638b1869d52f
ulysses-ck <ulisesapaza.280@gmail.com>
2023-11-22 00:57:47 -0300

    fix: bad setting argument

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,17 +1,18 @@
+from typing import Optional
 import pandas as pd
 from peewee import *
 from abc import ABCMeta
 from modelo_orm import *
 
 class GestionarObras(metaclass=ABCMeta):
-	df_obras_publicas = None
-	sqlite_db_obras = None
+	df_obras_publicas: pd.DataFrame = None
+	sqlite_db_obras: Optional[SqliteDatabase] = None
 
 	@classmethod
 	def extraer_datos(cls):
 		try:
-			df = pd.read_csv("./observatorio-de-obras-urbanas.csv")
-			cls.df = df
+			df = pd.read_csv("./observatorio-de-obras-urbanas.csv", sep=",")
+			return df
 		except FileNotFoundError as e:
 			print("El archivo no existe o la ubicación del mismo es incorrecta", e)
 			return None
@@ -21,18 +22,18 @@ class GestionarObras(metaclass=ABCMeta):
 		sqlite_db_obras = SqliteDatabase("./obras_urbanas.db")
 		try:
 			sqlite_db_obras.connect()
-			return sqlite_db_obras
+			cls.sqlite_db_obras = sqlite_db_obras
 		except OperationalError as e:
 			print("Se ha generado un error en la conexión a la BD.", e)
 			exit()
 
 	@classmethod
 	def mapear_orm(cls):
-		cls.sqlite_db_obras.create_tables([TipoAreaResponsable, TipoContratacion, Empresa, LicitacionOfertaEmpresa,Fechas, TipoObra, TipoEtapa, TipoEntorno, TipoComuna, TipoBarrio, Obra])
+		cls.sqlite_db_obras.create_tables([TipoAreaResponsable, TipoContratacion, Empresa, LicitacionOfertaEmpresa, Fechas, TipoObra, TipoEtapa, TipoEntorno, TipoComuna, TipoBarrio, Obra])
 
 	@classmethod
-	def limpiar_datos(cls, df):
-		cls.df = df.dropna(subset=["entorno", "nombre", "etapa", "tipo", "area_responsable", "monto_contrato", "comuna", "barrio", "direccion", "fecha_inicio", "porcenta_avance", "licitacion_oferta_empresa", "licitacion_anio", "contratacion_tipo", "nro_contratacion", "cuit_contratista", "mano_obra"], axis=0, inplace=True)
+	def limpiar_datos(cls):
+		cls.df_obras_publicas.dropna(subset=["entorno", "nombre", "etapa", "tipo", "area_responsable", "monto_contrato", "comuna", "barrio", "fecha_inicio", "licitacion_oferta_empresa", "contratacion_tipo", "nro_contratacion", "cuit_contratista"], axis=0, inplace=True)
 
 	@classmethod
 	def cargar_datos(cls):
@@ -58,3 +59,4 @@ class GestionarObras(metaclass=ABCMeta):
 		# i. Cantidad total de mano de obra empleada.
 		# j. Monto total de inversión. (atributo financiamiento)
 		print("obtener indicadores")
+
